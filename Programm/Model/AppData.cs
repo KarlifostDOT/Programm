e57d903efbff6f7f9f43262e9c6523458d1af41a
KarlifostDOT <131799464+KarlifostDOT@users.noreply.github.com>
2023-05-10 21:10:21 +0500

    Добавьте файлы проекта.

---
+++ Programm/Model/AppData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programm.Model
+{
+    public static class AppData
+    {
+        public static bdEntities db = new bdEntities();
+
+    }
+}
---
+++ Programm/Views/WelcomePage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Programm.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Programm.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для WelcomePage.xaml
+    /// </summary>
+    public partial class WelcomePage : Page
+    {
+        private readonly bdEntities dbContext = AppData.db;
+
+        public WelcomePage()
+        {
+            InitializeComponent();
+
+        }
+        private void WelcomePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this).Height = 833;
+            Window.GetWindow(this).Width = 1182;
+        }
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+                NavigationService.Navigate(new SignInPage());
+        }
+    }
+}
